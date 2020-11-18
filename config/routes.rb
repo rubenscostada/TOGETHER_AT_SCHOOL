eb9e3c63f071016811360dac5af7c5dd6869296d
Max Sevenig <max.sevenig@hotmail.com>
2020-11-18 12:40:47 +0000

    Some routes for pictures and diary

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :classes do
+    resources :pictures, only: [ :index, :new, :create]
+  end
+
+  resources :kids do
+    resources :small_events, only: [ :index, :new, :create]
+  end
 end
