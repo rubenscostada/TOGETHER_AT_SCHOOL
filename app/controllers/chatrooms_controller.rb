06ad804fd1ea30d6bef33b5d63281523b4f7f867
Max Sevenig <max.sevenig@hotmail.com>
2020-11-24 12:35:54 +0000

    Messenger finally works

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,9 +1,18 @@
 class ChatroomsController < ApplicationController
   def index
+  if current_user.admin?
     @chatrooms = Chatroom.all
+  else
+    @chatrooms = current_user.chatrooms
+  end
   end
 
   def show
+    if current_user.admin?
+      @chatrooms = Chatroom.all
+    else
+      @chatrooms = current_user.chatrooms
+    end
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new chatroom: @chatroom
     @messages = @chatroom.messages
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :messages
-  has_many :chatrooms, through: :messages
+  has_many :chatrooms, -> { distinct }, through: :messages
   has_many :classes, class_name: "::Classe"
   has_many :user_kids
   has_many :kids, through: :user_kids
